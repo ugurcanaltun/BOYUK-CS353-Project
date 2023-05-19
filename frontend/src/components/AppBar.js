868fd516733d88b4e954eb9455a164c7483f137c
Uğur Can Altun <82045097+ugurcanaltun@users.noreply.github.com>
2023-05-19 15:41:40 +0300

    appbar and drawer seperated

---
+++ frontend/src/components/AppBar.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+export default function AppNavbar(props) {
+    const handleDrawerOpen = () => {
+        props.setOpen(true);
+    };
+
+    return (
+        <AppBar position="absolute" open={props.open}>
+            <Toolbar
+                sx={{
+                pr: '24px', // keep right padding when drawer closed
+                }}
+            >
+                <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                sx={{
+                    marginRight: '36px',
+                    ...(props.open && { display: 'none' }),
+                }}
+                >
+                <MenuIcon />
+                </IconButton>
+                <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+                >
+                    Dashboard
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
