15dc72f8a30ea3d9fe6b289c0fc04826a35ba2cb
Uğur Can Altun <82045097+ugurcanaltun@users.noreply.github.com>
2023-05-19 16:47:13 +0300

    rotuer implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
 import * as React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
+import AddNewDrugScreen from './screens/AddNewDrugScreen';
+import AdminScreen from './screens/AdminScreen';
+import ListMedicinesScreen from './screens/ListMedicinesScreen';
+import MyPrescriptionScreen from './screens/MyPrescriptionsScreen';
+import PastOrdersScreen from './screens/PastOrdersScreen';
+import PharmaciesScreen from './screens/PharmaciesScreen';
+import WarehouseOrdersScreen from './screens/WarehouseOrdersScreen';
 
 function App() {
     return (
-        <HomeScreen/>
+        <Router>
+            <Routes>
+                <Route index element={<LoginScreen />} />
+                <Route path="login" element={<LoginScreen />} />
+                <Route path="register" element={<RegisterScreen />} />
+                <Route path="home" element={<HomeScreen />}>
+                    <Route index element={<ListMedicinesScreen />} />
+                    <Route path="listmedicines" element={<ListMedicinesScreen />} />
+                    <Route path="addnewdrug" element={<AddNewDrugScreen />} />
+                    <Route path="admin" element={<AdminScreen />} />
+                    <Route path="myprescription" element={<MyPrescriptionScreen />} />
+                    <Route path="pastorders" element={<PastOrdersScreen />} />
+                    <Route path="pharmacies" element={<PharmaciesScreen />} />
+                    <Route path="warehouseorders" element={<WarehouseOrdersScreen />} />
+                </Route>
+            </Routes>
+        </Router>
     );
   }
   
