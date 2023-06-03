34d7e15e68e1de44f670b01f0dbdfc81ef1ba74a
Onnrr <onur.uysal@ug.bilkent.edu.tr>
2023-06-04 02:53:29 +0300

    admin bitti mi yia

--- frontend/src/screens/AdminScreen.js
+++ frontend/src/screens/AdminScreen.js
@@ -8,14 +8,26 @@ import { UserAdmin } from '../components/UserAdmin';
 import { HospitalAdmin } from '../components/HospitalAdmin';
 import { PharmacyAdmin } from '../components/PharmacyAdmin';
 import { WarehouseAdmin } from '../components/WarehouseAdmin';
-
+import { Alert, Snackbar } from '@mui/material';
 
 export default function BasicSelect() {
   const [selected, setSelected] = React.useState(2);
+  const [success, setSuccess] = React.useState(false);
+  const [snackOpen, setSnackOpen] = React.useState(false)
 
   const handleChange = (event) => {
     setSelected(event.target.value);
   };
+
+  React.useEffect(()=>{
+    if (success) {
+      setSnackOpen(true)
+    }
+  }, [success])
+
+  function handleSnackClose() {
+    setSnackOpen(false)
+  }
 
   return (
     <div>
@@ -40,11 +52,16 @@ export default function BasicSelect() {
         </FormControl>
     </Box>
     <div>
-      {(selected === 1)? <UserAdmin/>:null}
-      {(selected === 2)? <HospitalAdmin/>:null}
-      {(selected === 3)? <PharmacyAdmin/>:null}
-      {(selected === 4)? <WarehouseAdmin/>:null}
+      {(selected === 1)? <UserAdmin setSuccess={setSuccess}/>:null}
+      {(selected === 2)? <HospitalAdmin setSuccess={setSuccess}/>:null}
+      {(selected === 3)? <PharmacyAdmin setSuccess={setSuccess}/>:null}
+      {(selected === 4)? <WarehouseAdmin setSuccess={setSuccess}/>:null}
     </div>
+    <Snackbar open={snackOpen} autoHideDuration={4000} onClose={handleSnackClose} anchorOrigin={{ vertical:"bottom",horizontal:"right" }}>
+          <Alert onClose={handleSnackClose} severity="success" sx={{ width: '100%' }}>
+              Operation Successful
+          </Alert>
+      </Snackbar>
     </div>
   );
 }
