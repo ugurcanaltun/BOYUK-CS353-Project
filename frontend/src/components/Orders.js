bed3d04e15fdefc657fe83847601dbfea4977bb2
Onnrr <onur.uysal@ug.bilkent.edu.tr>
2023-06-03 22:24:24 +0300

    home orders fix

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -6,81 +6,64 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
-
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
-}
-
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
-];
-
-function preventDefault(event) {
-  event.preventDefault();
-}
+import { useEffect, useState } from "react";
+import { fetchOrders } from "../api/OrdersAPI";
+import { Button } from "@mui/material";
 
 export default function Orders() {
+  const [orders, setOrders] = useState([])
+    useEffect(()=> {
+        fetchOrders().then(o=> {
+            console.log(o)
+            setOrders(o)
+        })
+    }, [])
+
+  function OrderItem(props) {
+    return (
+        <TableRow>
+            <TableCell component="th" scope="row">{props.date}</TableCell>
+            <TableCell component="th" scope="row">{props.name}</TableCell>
+            <TableCell component="th" scope="row">{props.accNo}</TableCell>
+            <TableCell component="th" scope="row">{props.count}</TableCell>
+            <TableCell component="th" scope="row">${props.price}</TableCell>
+        </TableRow>
+    )
+  }
   return (
     <React.Fragment>
       <Title>Recent Orders</Title>
+      {
+      (orders && orders.length > 0)?
       <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Drug Name</TableCell>
+            <TableCell>Bank Account No</TableCell>
+            <TableCell>Number of Items</TableCell>
+            <TableCell align="right">Total Price</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
-            </TableRow>
-          ))}
+          {
+              orders?
+              orders.map(o=> {
+                  return <OrderItem 
+                      date={o.order_date}
+                      name={o.drug_name} 
+                      accNo={o.bank_account_no} 
+                      count={o.count} 
+                      price={o.total_price} 
+                      />
+              }):<></>
+          }
         </TableBody>
-      </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+      </Table>: <h4>You have no previous orders</h4>
+      }
+      <Button color="primary" href="/home/pastorders" sx={{ mt: 3 }}>
         See more orders
-      </Link>
+      </Button>
     </React.Fragment>
   );
 }
--- frontend/src/screens/MyPrescriptionsScreen.js
+++ frontend/src/screens/MyPrescriptionsScreen.js
@@ -1,18 +1,71 @@
 import { useEffect, useState } from "react";
+import { fetchOrders } from "../api/OrdersAPI";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button } from "@mui/material";
 import { fetchPatientPrescriptions } from "../api/PrescriptionAPI";
 
-export default function MyPrescriptionScreen() {
+export default function MyPrescriptionsScreen() {
     const [prescriptions, setPrescriptions] = useState([])
-
-    useEffect(() =>{
-        fetchPatientPrescriptions().then(p=>{
-            setPrescriptions(p)
+    useEffect(()=> {
+        fetchPatientPrescriptions().then(o=> {
+            console.log(o)
+            setPrescriptions(o)
         })
     }, [])
-
+    function PrescriptionItem(props) {
+        return (
+            <TableRow>
+                <TableCell component="th" scope="row">{props.date}</TableCell>
+                <TableCell component="th" scope="row">{props.name}</TableCell>
+                <TableCell component="th" scope="row">{props.accNo}</TableCell>
+                <TableCell component="th" scope="row">{props.count}</TableCell>
+                <TableCell component="th" scope="row">${props.price}</TableCell>
+            </TableRow>
+        )
+    }
     return(
-        <div className="patient-prescription-container">
-            My Prescription
+        <div className="orders-container">
+            {
+                (prescriptions && prescriptions.length > 0)?
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Prescription Date</TableCell>
+                            <TableCell>Drug Name</TableCell>
+                            <TableCell align="right">Doctor Name</TableCell>
+                            <TableCell align="right">Illness</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                prescriptions?
+                                prescriptions.map(p=> {
+                                    return <PrescriptionItem 
+                                        date={p.order_date}
+                                        name={p.drug_name} 
+                                        accNo={p.bank_account_no} 
+                                        count={p.count} 
+                                        price={p.total_price} 
+                                        />
+                                }):<></>
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                :
+                <>
+                <h4>You have no previous orders</h4>
+                <Button href="/home/listmedicines">Go to medicine list</Button>
+                </>
+            }
+            
         </div>
     );
 }
